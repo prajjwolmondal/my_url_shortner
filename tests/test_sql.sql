b9173544246d820a876c81e9c931c6941d98bc52
Prajjwol Mondal <prajj.mondal@gmail.com>
2020-08-27 14:28:47 -0400

    updated test_sql

--- tests/test_sql.sql
+++ tests/test_sql.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS `urllist`;
 CREATE TABLE urllist (
     short_code varchar(6),
     full_url varchar(120),
-    created_at datetime
+    created_at datetime,
+    created_by varchar(32)
 );
 
 INSERT INTO url_shortcodes.urllist
 (short_code, full_url, created_at)
-VALUES('ILAMMO', 'https://www.linkedin.com/in/prajjwolmondal/', '2020-08-07 03:46:12.0');
+VALUES('ILAMMO', 'https://www.linkedin.com/in/prajjwolmondal/', '2020-08-07 03:46:12.0', None);
 
 INSERT INTO url_shortcodes.urllist
 (short_code, full_url, created_at)
-VALUES('ULOTIG', 'https://github.com/prajjwolmondal/', '2020-07-28 03:50:14.0');
+VALUES('ULOTIG', 'https://github.com/prajjwolmondal/', '2020-07-28 03:50:14.0', None);
